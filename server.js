627895dc36fb02702a156a29ff3a8e118dfd00d6
Nolanws1 <Nolanws@gmail.com>
2020-11-06 09:32:45 -0800

    trying to fix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static("public"));
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({ extended : true }))
 app.use(express.json())
 
 var exphbs = require("express-handlebars");
