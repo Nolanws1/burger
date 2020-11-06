22da955f94ea794f74ed7d662b1d03445d901ac9
Nolanws1 <Nolanws@gmail.com>
2020-11-05 21:38:11 -0800

    Complete Functionality

--- models/burger.js
+++ models/burger.js
@@ -18,7 +18,14 @@ var burger = {
       cb(res);
     });
   },
+  delete: function(condition, cb) {
+    orm.delete("burgers", condition, function(res) {
+      cb(res);
+    });
+  }
 };
+
+
 
 
 module.exports = burger;
