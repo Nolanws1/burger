892c35a38e4bba5314b15bc8880f804379af4d61
Nolanws1 <Nolanws@gmail.com>
2020-11-05 21:23:34 -0800

    Everything works now, but I'm still going to make it better

--- public/assets/js/burgerpublic.js
+++ public/assets/js/burgerpublic.js
@@ -1,4 +1,26 @@
 $(function () {
+
+  $(".change-toEaten").on("click", function(event) {
+
+    event.preventDefault();
+
+    var id = $(this).data("id");
+    var eaten = 1;
+
+    var nowEaten = {
+      devoured: eaten
+    };
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: nowEaten
+    }).then(
+      function() {
+        console.log("changed sleep to", nowEaten);
+        location.reload();
+      }
+    );
+  });
+
   $(".create-form").on("submit", function (event) {
 
     event.preventDefault();
